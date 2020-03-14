48b27cdc189d75f1689bb3d233d4d89128cd2d46
Ellaqi <ellaqi77777@gmail.com>
2020-03-13 22:37:25 -0400

    footer 1st commit

--- index-header.php
+++ index-header.php
@@ -1,1 +1,32 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>BerryTeam</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style/uniform.css">
+    </head>
+    <body>
+    <!----------- header ------------>
+    <header id="header">
+
+        <div class="logo h-logo">
+            <a href=""><img class="logoimg" src="img/logo.png" alt="logo"></a>
+        </div>
+
+        <div class="log">
+            <button class="login">
+                <a href="">Log in</a>
+            </button>
+            <button class="signup">
+                <a href="">Sign up</a>
+            </button>
+        </div>
+        <div class="faq">
+            <a href="">FAQ</a>
+        </div>
+
+    </header>
