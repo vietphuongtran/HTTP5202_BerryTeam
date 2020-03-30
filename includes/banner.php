a295ca5343c4e41108ebd2c07f770f941a519d80
Ellaqi <ellaqi77777@gmail.com>
2020-03-30 16:20:37 -0400

    change index page styling

---
+++ includes/banner.php
@@ -0,0 +1,9 @@
+<?php
+?>
+
+<!------------ banner ------------>
+<div id="banner">
+    <div class="quote">
+        <h2>Talent wins games,<br><span class="banner-text">but teamwork and intelligence win championships.</span></h2>
+    </div>
+</div>
