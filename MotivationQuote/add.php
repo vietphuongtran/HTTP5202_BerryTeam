318ef3f537605353ba73c858e42fa5aff1778589
Ellaqi <ellaqi77777@gmail.com>
2020-04-05 14:25:45 -0400

    validation updated

--- MotivationQuote/add.php
+++ MotivationQuote/add.php
@@ -10,22 +10,29 @@ use Classes\Database as dbConnect;
 //    public function add($coll);
 //}
 
-$quoteerr = "";
+$errormsg = "";
 if(isset($_POST['addMotiQuotes'])) {
     $quotes = $_POST['quotes'];
     $category = $_POST['category'];
-    //error messages
-    if ($quotes == "") {
-        $quoteerr = "Required field";
-    } else {
-        $quoteerr = "";
-    }
 
     //new database connection
     $dbcon = dbConnect::getDb();
     //new instance of quotes class
     $quote = new allmotiquotes();
     $count = $quote->addMotiQuotes($dbcon, $quotes, $category);
+
+    //validation
+    function validation ($input) {
+        if ($input === "") {
+            global $errormsg;
+            $errormsg = "Required field";
+        } else {
+            $errormsg = "";
+        }
+        return $errormsg;
+    }
+    validation($quote);
+
     if ($count) {
         header("Location: list.php");
     } else {
@@ -40,25 +47,23 @@ if(isset($_POST['addMotiQuotes'])) {
         <meta name="description" content="Berryteam System">
         <meta name="keywords" content="Berryteam, Motivation, Quotes">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="/CSS/main.css" type="text/css">
 
-        <link rel="stylesheet" href="/Stylesheets/taskcrud.css">
         <link rel="stylesheet" href="/Stylesheets/uniform.css">
         <link rel="stylesheet" type="text/css" href="/Stylesheets/navigation.css">
     </head>
     <body>
-        <? include '../includes/header-index.php' ?>
+        <? include '../includes/header-landing.php' ?>
         <? include '../includes/navigation.php' ?>
         <div>
             <h1>Create your own motivation quotes</h1>
             <!--    Form to Add a Motivation Quote    -->
             <form action="" method="post">
                 <div class="form-group">
-                    <label for="quotes">Input your motivation quote </label>
+                    <label for="quotes">Type your motivation quote </label>
                     <input type="text" class="form-control" name="quotes" id="quotes" value=""
                            placeholder="Add your customized motivation quote here">
                     <span style="color: red">
-                        <?= $quoteerr; ?>
+                        <?= $errormsg; ?>
                     </span>
                 </div>
                 <div class="form-group">
