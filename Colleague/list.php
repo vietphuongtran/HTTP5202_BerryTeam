b05906ebcbfe54f63903a48f96f722fa37554a48
Ellaqi <ellaqi77777@gmail.com>
2020-04-05 14:21:12 -0400

    maincontainer div added

--- Colleague/list.php
+++ Colleague/list.php
@@ -28,56 +28,62 @@ $searchContent = "";
     <body>
         <? include '../includes/header-landing.php' ?>
         <? include '../includes/navigation.php' ?>
-        <p class="h1 text-center">All team members</p>
-        <!--    search colleagues   -->
-        <div class="s-1">
-            <form action="search.php" method="post">
-                <input type="text" name="searchContent" value="<?= $searchContent; ?>">
-                <input type="submit" class="button btn btn-primary" name="searchColleague" value="Search"/>
-            </form>
-        </div>
+        <div class="maincontainer">
+            <p class="h1 text-center">All team members</p>
+            <!--    search colleagues   -->
+            <div class="s-1">
+                <form action="search.php" method="post">
+                    <input type="text" name="searchContent" value="<?= $searchContent; ?>">
+                    <input type="submit" class="button btn btn-primary" name="searchColleague" value="Search"/>
+                </form>
+                <select>
+                    <option>--sort by--</option>
+                    <option>Department</option>
+                    <option>First name</option>
+                    <option>Last name</option>
+                </select>
+            </div>
 
-        <div class="m-1">
-            <!--    Displaying Data in Table-->
-            <table class="table table-bordered tbl">
-                <thead>
-                <tr>
-                    <th scope="col"></th>
-                    <th scope="col">First Name</th>
-                    <th scope="col">Last Name</th>
-                    <th scope="col">Department</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Update</th>
-                    <th scope="col">Delete</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($colleagues as $colleague) { ?>
+            <div class="m-1">
+                <!--    Displaying Data in Table-->
+                <table class="table table-bordered tbl">
+                    <thead>
                     <tr>
-                        <th><?= $colleague['id'] ?></th>
-                        <td><?= $colleague['fname'] ?></td>
-                        <td><?= $colleague['lname'] ?></td>
-                        <td><?= $colleague['department'] ?></td>
-                        <td><?= $colleague['phone'] ?></td>
-                        <td><?= $colleague['email'] ?></td>
-                        <td>
-                            <form action="update.php" method="post">
-                                <input type="hidden" name="uid" value="<?= $colleague['id'] ?>"/>
-                                <input type="submit" class="button btn btn-primary" name="updateColleague" value="Update"/>
-                            </form>
-                        </td>
-                        <td>
-                            <form action="delete.php" method="post">
-                                <input type="hidden" name="id" value="<?= $colleague['id'] ?>"/>
-                                <input type="submit" class="button btn btn-danger" name="deleteColleague" value="Delete"/>
-                            </form>
-                        </td>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Department</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Update</th>
+                        <th scope="col">Delete</th>
                     </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-            <a href="add.php" id="btn_addColleague" class="btn btn-success btn-lg float-right">New Colleague</a>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($colleagues as $colleague) { ?>
+                        <tr>
+                            <td><?= $colleague['fname'] ?></td>
+                            <td><?= $colleague['lname'] ?></td>
+                            <td><?= $colleague['department'] ?></td>
+                            <td><?= $colleague['phone'] ?></td>
+                            <td><?= $colleague['email'] ?></td>
+                            <td>
+                                <form action="update.php" method="post">
+                                    <input type="hidden" name="uid" value="<?= $colleague['id'] ?>"/>
+                                    <input type="submit" class="button btn btn-primary" name="updateColleague" value="Update"/>
+                                </form>
+                            </td>
+                            <td>
+                                <form action="delete.php" method="post">
+                                    <input type="hidden" name="id" value="<?= $colleague['id'] ?>"/>
+                                    <input type="submit" class="button btn btn-danger" name="deleteColleague" value="Delete"/>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+                <a href="add.php" id="btn_addColleague" class="btn btn-success btn-lg float-right">New Colleague</a>
+            </div>
         </div>
         <? include '../includes/footer-landing.php' ?>
     </body>
