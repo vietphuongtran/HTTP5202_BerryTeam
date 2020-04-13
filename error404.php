20f2bcde5734d72479fb8debe364bb9872feaf04
Ellaqi <ellaqi77777@gmail.com>
2020-04-13 11:34:13 -0400

    404 update

--- error404.php
+++ error404.php
@@ -10,17 +10,16 @@
     <meta name="keywords" content="Berryteam, motivation, quote">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/main.css" type="text/css">
 
-    <link rel="stylesheet" href="Stylesheets/taskcrud.css">
-    <link rel="stylesheet" href="Stylesheets/uniform.css">
-    <link rel="stylesheet" type="text/css" href="Stylesheets/navigation.css">
+    <link rel="stylesheet" href="../Stylesheets/uniform.css">
+    <link rel="stylesheet" type="text/css" href="../Stylesheets/navigation.css">
 </head>
 <body>
-<? include 'includes/header-index.php' ?>
-<? include 'includes/navigation.php' ?>
+<? include '../includes/header-landing.php' ?>
+<? include '../includes/navigation.php' ?>
 <div class="maincontainer">
     <h1>404 NOT FOUND...</h1>
+<!--    <div>--><?//= $msg; ?><!--</div>-->
 </div>
 <? include 'includes/footer-landing.php' ?>
 </body>
