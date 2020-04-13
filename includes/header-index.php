12fa239e7726333bd8aa2463909dda369eb50cd2
Ellaqi <ellaqi77777@gmail.com>
2020-04-13 11:42:08 -0400

    server test

--- includes/header-index.php
+++ includes/header-index.php
@@ -1,24 +1,24 @@
 <?php
 ?>
 
-    <!----------- header ------------>
-    <header id="header">
-        <div class="logo h-logo">
-            <a href=""><img class="logoimg" src="img/logo.png" alt="logo"></a>
-        </div>
-        <div class="log">
-            <button class="login">
-                <a href="">Log in</a>
-            </button>
-            <button class="signup">
-                <a href="">Sign up</a>
-            </button>
+<!----------- header ------------>
+<header id="header">
+    <div class="logo h-logo">
+        <a href=""><img class="logoimg" src="img/logo.png" alt="logo"></a>
+    </div>
+    <div class="log">
+        <button class="login">
+            <a href="">Log in</a>
+        </button>
+        <button class="signup">
+            <a href="registration/addRegistration.php">Sign up</a>
+        </button>
 
-            <button class="faq">
-                <a href="">FAQ</a>
-            </button>
-        </div>
-    </header>
+        <button class="faq">
+            <a href="faq/listfaq.php">FAQ</a>
+        </button>
+    </div>
+</header>
 
 
 
--- includes/header-landing.php
+++ includes/header-landing.php
@@ -6,10 +6,10 @@ $gdate = date("m-d-Y") . ",  " . date("l");
 <!----------- header ------------>
 <header id="la-header">
     <div class="la-logo">
-        <a href="/index.php"><img class="la-logoimg" src="/img/logo.png" alt="logo"></a>
+        <a href="../index.php"><img class="la-logoimg" src="../img/logo.png" alt="logo"></a>
     </div>
     <div class="la-greeting">
-        Hi, Ella Qi ! <?= $username; ?> <span>Today is <?= $gdate; ?>.</span>
+        Hi ! <?= $username; ?> <span>Today is <?= $gdate; ?>.</span>
     </div>
     <div class="la-log">
         <button class="la-logout">
@@ -17,7 +17,7 @@ $gdate = date("m-d-Y") . ",  " . date("l");
         </button>
 
         <button class="la-faq">
-            <a href="/faq/listfaq.php">FAQ</a>
+            <a href="../faq/listfaq.php">FAQ</a>
         </button>
     </div>
 </header>
