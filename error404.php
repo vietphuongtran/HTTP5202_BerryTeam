28cc30cafe8dcb43db1b3845bfc5fce23ce6746a
Ellaqi <ellaqi77777@gmail.com>
2020-04-13 11:38:02 -0400

    server test

--- error404.php
+++ error404.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="Berryteam, motivation, quote">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../Stylesheets/error404.css">
     <link rel="stylesheet" href="../Stylesheets/uniform.css">
     <link rel="stylesheet" type="text/css" href="../Stylesheets/navigation.css">
 </head>
