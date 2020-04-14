ce4ab8c3dbd0872e86db11443a7a86158aa54e26
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-04-13 20:51:42 -0400

    update comments sql table

--- Database/SQL Table/comments.sql
+++ Database/SQL Table/comments.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2020 at 07:13 PM
+-- Generation Time: Apr 14, 2020 at 12:50 AM
 -- Server version: 5.7.24-log
 -- PHP Version: 7.2.10
-
--- NOTE: add using database "berry_team":
-USE berry_team;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comments` (
-  `content` varchar(100) NOT NULL,
+  `comment` varchar(100) NOT NULL,
   `discussion_id` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `colleague_id` int(11) DEFAULT NULL
@@ -42,8 +39,7 @@ CREATE TABLE `comments` (
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`content`, `discussion_id`, `id`, `colleague_id`) VALUES
-('Yes, let\'s do that', 2, 1, 1),
+INSERT INTO `comments` (`comment`, `discussion_id`, `id`, `colleague_id`) VALUES
 ('No, don\'t do that. Data leak is a real problem', 1, 2, 1),
 ('We cancel it, none of us has children but your child is always welcome anytime', 3, 3, 2),
 ('I \'d love that, I live in Mississauga and that\'s great news', 2, 4, 2);
@@ -68,7 +64,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
